5999eb35ea5c9cba8b0c5bc5be312f411b8d3721
Mikhail A <mg@abramishvili.net>
2021-12-09 02:57:17 +0500

    .

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -20,119 +20,119 @@
                 panel
             </div>-->
         @endauth
-
-        <div id="app">
-            <header>
-                <div class="header-top">
-                    <div class="container">
-                        <div class="row align-items-center">
-                            <div class="header-top-nav">
-                                <nav>
-                                    <ul class="nav">
-                                        <li class="nav-item">
-                                            <a href="#" class="nav-link">О компании</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="#" class="nav-link">Доставка и оплата</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="{{ route('page', ['slug' => 'contacts'], false) }}" class="nav-link">Контакты</a>
-                                        </li>
-                                    </ul>
-                                </nav>
-                            </div>
-                            <div class="header-top-search">
-                                <form action="{{ route('product_search', [], false) }}">
-                                    <input name="search" type="text" class="form-control" placeholder="Поиск по магазину..." required>
-                                    <input type="submit" value="">
-                                </form>
-                            </div>
+        <header>
+            <div class="header-top">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="header-top-nav">
+                            <nav>
+                                <ul class="nav">
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">О компании</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">Доставка и оплата</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="{{ route('page', ['slug' => 'contacts'], false) }}" class="nav-link">Контакты</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div class="header-top-search">
+                            <form action="{{ route('product_search', [], false) }}">
+                                <input name="search" type="text" class="form-control" placeholder="Поиск по магазину..." required>
+                                <input type="submit" value="">
+                            </form>
                         </div>
                     </div>
                 </div>
-                <div class="header-bottom">
-                    <div class="container">
-                        <div class="row align-items-center">
-                            <div class="header-bottom-logo">
-                                <a href="{{ route('home', [], false) }}">
-                                    <img src="/img/logo.svg" alt="">
-                                </a>
-                            </div>
-                            <div class="header-bottom-catalog">
-                                <a href="{{ route('categories', [], false) }}">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-                                        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+            </div>
+            <div class="header-bottom">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="header-bottom-logo">
+                            <a href="{{ route('home', [], false) }}">
+                                <img src="/img/logo.svg" alt="">
+                            </a>
+                        </div>
+                        <div class="header-bottom-catalog">
+                            <a href="{{ route('categories', [], false) }}">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                                </svg>
+                                <span>Каталог</span>
+                            </a>
+                        </div>
+                        <div class="header-bottom-tel">
+                            <div class="header-bottom-tel-wrapper">
+                                <div class="header-bottom-tel-icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+                                        <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
                                     </svg>
-                                    <span>Каталог</span>
-                                </a>
-                            </div>
-                            <div class="header-bottom-tel">
-                                <div class="header-bottom-tel-wrapper">
-                                    <div class="header-bottom-tel-icon">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
-                                            <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
-                                        </svg>
-                                    </div>
-                                    <a href="tel:+7{!! substr(str_replace(array(' ', '-', '+'), '', App\Http\Controllers\SettingController::index()->tel), 1) !!}">
-                                        <small>{{App\Http\Controllers\SettingController::index()->schedule}}</small>
-                                        {{App\Http\Controllers\SettingController::index()->tel}}
-                                    </a>
                                 </div>
-                            </div>
-                            <div class="header-bottom-cart">
-                                <mini-cart></mini-cart>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!--
-                <div class="header-top">
-                    <div class="container">
-                        <div class="row align-items-center">
-                            <div class="header-top-tel">
-                                <a>{{App\Http\Controllers\SettingController::index()->email}}</a>
-                                <a href="tel: +7{!! substr(str_replace(array(' ', '-', '+'), '', App\Http\Controllers\SettingController::index()->tel), 1) !!}">
+                                <a href="tel:+7{!! substr(str_replace(array(' ', '-', '+'), '', App\Http\Controllers\SettingController::index()->tel), 1) !!}">
+                                    <small>{{App\Http\Controllers\SettingController::index()->schedule}}</small>
                                     {{App\Http\Controllers\SettingController::index()->tel}}
                                 </a>
                             </div>
-                            <div class="header-top-search">
-                                <form action="{{ route('product_search', [], false) }}">
-                                    <input name="search" type="text" class="form-control" placeholder="Поиск по магазину..." required>
-                                    <input type="submit" value="">
-                                </form>
-                            </div>
+                        </div>
+                        <div class="header-bottom-cart">
+                            <mini-cart></mini-cart>
                         </div>
                     </div>
                 </div>
-                <div class="header-bottom">
-                    <div class="container">
-                        <div class="row align-items-center">
-                            <div class="header-bottom-logo">
-                                <a href="{{ route('home', [], false) }}">
-                                    <img src="/img/logo.svg" alt="TachLab - ТачЛаб">
-                                </a>
-                            </div>
-                            <div class="header-bottom-nav">
-                                <nav>
-                                    <ul class="nav">
-                                        <li class="nav-item">
-                                            <a href="{{ route('categories', [], false) }}" class="nav-link">Магазин</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="{{ route('page', ['slug' => 'contacts'], false) }}" class="nav-link">Контакты</a>
-                                        </li>
-                                    </ul>
-                                </nav>
-                            </div>
-                            <div class="header-bottom-cart">
-                                <a href="{{ route('cart', [], false) }}">
-                                    <minicart></minicart>
-                                </a>
-                            </div>
+            </div>
+            <!--
+            <div class="header-top">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="header-top-tel">
+                            <a>{{App\Http\Controllers\SettingController::index()->email}}</a>
+                            <a href="tel: +7{!! substr(str_replace(array(' ', '-', '+'), '', App\Http\Controllers\SettingController::index()->tel), 1) !!}">
+                                {{App\Http\Controllers\SettingController::index()->tel}}
+                            </a>
+                        </div>
+                        <div class="header-top-search">
+                            <form action="{{ route('product_search', [], false) }}">
+                                <input name="search" type="text" class="form-control" placeholder="Поиск по магазину..." required>
+                                <input type="submit" value="">
+                            </form>
                         </div>
                     </div>
-                </div>-->
-            </header>
+                </div>
+            </div>
+            <div class="header-bottom">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="header-bottom-logo">
+                            <a href="{{ route('home', [], false) }}">
+                                <img src="/img/logo.svg" alt="TachLab - ТачЛаб">
+                            </a>
+                        </div>
+                        <div class="header-bottom-nav">
+                            <nav>
+                                <ul class="nav">
+                                    <li class="nav-item">
+                                        <a href="{{ route('categories', [], false) }}" class="nav-link">Магазин</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="{{ route('page', ['slug' => 'contacts'], false) }}" class="nav-link">Контакты</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div class="header-bottom-cart">
+                            <a href="{{ route('cart', [], false) }}">
+                                <minicart></minicart>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>-->
+        </header>
+        <div id="app">
+            
             
             @yield('content')
 
