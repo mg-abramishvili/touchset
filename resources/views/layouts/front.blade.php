acc7e98263747dd215f7638d5b9b64fcfa73f22a
Mikhail A <mg@abramishvili.net>
2021-12-09 17:37:05 +0500

    .

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -145,7 +145,7 @@
             </footer>
         </div>
         
-        <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
+        {{-- <script src="{{ mix('js/app.js') }}" type="text/javascript"></script> --}}
         @yield('scripts')
     </body>
 </html>
