ecc32a155005224ac992335c27ff6ea1ce94d12a
Mikhail A <mg@abramishvili.net>
2021-12-09 18:22:56 +0500

    .

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -36,7 +36,7 @@
                                             <a href="#" class="nav-link">Доставка и оплата</a>
                                         </li>
                                         <li class="nav-item">
-                                            <a href="{{ route('page', ['slug' => 'contacts'], false) }}" class="nav-link">Контакты</a>
+                                            <a href="{{ route('page', ['slug' => 'contacts'], false) }}/" class="nav-link">Контакты</a>
                                         </li>
                                     </ul>
                                 </nav>
@@ -59,7 +59,7 @@
                                 </a>
                             </div>
                             <div class="header-bottom-catalog">
-                                <a href="{{ route('categories', [], false) }}">
+                                <a href="{{ route('categories', [], false) }}/">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
                                         <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
                                     </svg>
