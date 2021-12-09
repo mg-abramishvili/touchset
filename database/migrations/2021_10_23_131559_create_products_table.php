c68005a9bb095942d20be052ff2b449c0e626a09
Mikhail A <mg@abramishvili.net>
2021-12-10 04:19:20 +0500

    .

--- database/migrations/2021_10_23_131559_create_products_table.php
+++ database/migrations/2021_10_23_131559_create_products_table.php
@@ -11,6 +11,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('slug');
             $table->longText('description')->nullable();
             $table->longText('gallery')->nullable();
             $table->decimal('price')->default(0);
