3e1664aa1fc3d43c10edff5ad54007c1771381d1
Mikhail A <mg@abramishvili.net>
2021-12-09 17:37:49 +0500

    .

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -12,7 +12,7 @@
         <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet"/>
     </head>
     <body class="admin_panel" style="min-height: 100vh;">
-        <div id="app">
+        <div id="appa">
             <div class="container-fluid">
                 <div class="row">
                     @auth
@@ -124,7 +124,7 @@
             </div>
         </div>
         
-        {{-- <script src="{{ mix('js/app.js') }}" type="text/javascript"></script> --}}
+        <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
         @yield('scripts')
     </body>
 </html>
