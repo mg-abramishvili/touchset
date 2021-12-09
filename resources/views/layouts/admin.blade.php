3f5abedb90fafd5b788503958ce9e6949dc7bb1b
Mikhail A <mg@abramishvili.net>
2021-12-09 14:12:07 +0500

    .

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -126,7 +126,7 @@
             </div>
         </div>
         
-        {{-- <script src="{{ mix('js/app.js') }}" type="text/javascript"></script> --}}
+        <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
         @yield('scripts')
     </body>
 </html>
