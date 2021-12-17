fcae0bfd0261e329e4ea582a48e91bb28edfd52a
Mikhail A <mg@abramishvili.net>
2021-12-17 14:45:37 +0500

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.front')
 @section('title', 'ТачЛаб')
 @section('content')
-<input type="file" name="" id="">
+<input type="file">
 <div class="home-carousel">
     <div class="container">
         <div class="row">
@@ -9,7 +9,7 @@
                 <div class="home-carousel-1">
                     <div class="home-carousel-inner">
                         <h1>Программное обеспечение для информационных киосков</h1>
-                        <a href="{{ route('categories') }}" class="btn btn-standard">
+                        <a href="{{ route('categories', [], false) }}" class="btn btn-standard">
                             В каталог
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
